b72eab04cc9b1b012cdf68d1486c285c6d17e63d
FuckDerry <467165887@qq.com>
2017-03-01 15:03:55 +0800

    thomas

---
+++ thomas/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace thomas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(251, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始执行";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(122, 52);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 36);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(351, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBox2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.textBox2.Location = new System.Drawing.Point(12, 94);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(423, 228);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("宋体", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(170, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 50);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "9999-99-99 99:99:99";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(49, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 35);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "现在时间：";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(6, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 36);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "定时时间：";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 325);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+    }
+}
+
---
+++ thomas/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace thomas
+{
+    public partial class Form1 : Form
+    {
+        public int pos = -1;
+        public int i = 0;
+        public int temp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SendEmail("Thomas", Mysql());
+            //temp=Convert.ToInt32(textBox1.Text);//获取时间间隔
+            //i = 0;//设置临时变量
+            if (textBox1.Text.ToString() != "")
+            {
+                pos = 1;//标志位1：开始计时
+                output("邮件定时发送软件启动！！！");
+                output("您设定的时间为:" + textBox1.Text);
+            }
+            else
+            {
+                output("请输入定时时间！！！");
+            }
+
+
+        }
+
+        public void SendEmail(String title, String content)
+        {
+            try
+            {
+                MailMessage mailMsg = new MailMessage();
+                mailMsg.From = new MailAddress("m18395587763@163.com", "m18395587763@163.com");
+                mailMsg.To.Add(new MailAddress("467165887@qq.com", "467165887@qq.com"));
+                mailMsg.Subject = title;
+                mailMsg.Body = content;
+                SmtpClient client = new SmtpClient("smtp.163.com", 25);  //发送服务器
+                client.Credentials = (ICredentialsByHost)new NetworkCredential("m18395587763@163.com", "Cdefgab960308");
+                client.Send(mailMsg);
+                output("发送成功！！！");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("异常" + ex.StackTrace);
+            }
+        }
+        public string Mysql()
+        {
+            string content = "";
+            string lateDate = DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd");
+            string connectionString = string.Format("Server = {0}; Database = {1}; User ID = {2}; Password = {3};", host, database, id, pwd);
+            openSqlConnection(connectionString);
+
+            MySqlCommand mySqlCommand2 = new MySqlCommand("UPDATE idiot SET sent=1 where time like \"" + lateDate + "%\";", dbConnection);
+            mySqlCommand2.ExecuteNonQuery();
+            MySqlCommand mySqlCommand = new MySqlCommand("Select * from idiot where time like \"" + lateDate + "%\";", dbConnection);
+            //MySqlDataReader reader = mySqlCommand.ExecuteReader();
+            try
+            {
+                DataSet data = GetDataSet("Select * from idiot where time like \"" + lateDate + "%\";");
+                foreach (DataRow row in data.Tables[0].Rows)
+                {
+                    content += "------------------------------------------------\n"
+                               + "编号：" + row["id"].ToString() + "\n"
+                               + "标签：" + row["label"].ToString() + "\n"
+                               + "时间：" + row["time"].ToString() + "\n"
+                               + "地点：" + row["location"].ToString() + "\n"
+                               + "标题：" + row["title"].ToString() + "\n"
+                               + "内容：" + "\n" + Read(row["content"].ToString()) + "\n"
+                               + "------------------------------------------------\n";
+                }
+            }
+            catch (Exception)
+            {
+                output("查询失败了！");
+            }
+            finally
+            {
+                // reader.Close();
+
+            }
+
+            MyObj = GetDataSet(strCommand);
+            return content;
+        }
+
+
+        //读取指定路径下的内容
+        public static string Read(string path)
+        {
+            string content = "";
+            StreamReader sr = new StreamReader(path, Encoding.UTF8);
+            content = sr.ReadToEnd();
+            return content;
+        }
+
+        // Global variables  
+        public static MySqlConnection dbConnection;//Just like MyConn.conn in StoryTools before  
+        static string host = "120.27.47.20";
+        static string id = "Derry";
+        static string pwd = "960308";
+        static string database = "thomas";
+        static string result = "";
+
+        private static string strCommand = "Select id from idiot ;";
+        public static DataSet MyObj;
+
+
+        // On quit  
+        public static void OnApplicationQuit()
+        {
+            closeSqlConnection();
+        }
+
+        // Connect to database  
+        private static void openSqlConnection(string connectionString)
+        {
+            dbConnection = new MySqlConnection(connectionString);
+            dbConnection.Open();
+            result = dbConnection.ServerVersion;
+            //Debug.Log("Connected to database."+result);  
+        }
+
+        // Disconnect from database  
+        private static void closeSqlConnection()
+        {
+            dbConnection.Close();
+            dbConnection = null;
+            //Debug.Log("Disconnected from database."+result);  
+        }
+
+        // MySQL Query  
+        public static void doQuery(string sqlQuery)
+        {
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+            dbCommand.CommandText = sqlQuery;
+            IDataReader reader = dbCommand.ExecuteReader();
+            reader.Close();
+            reader = null;
+            dbCommand.Dispose();
+            dbCommand = null;
+        }
+        #region Get DataSet
+        public static DataSet GetDataSet(string sqlString)
+        {
+            //string sql = UnicodeAndANSI.UnicodeAndANSI.UnicodeToUtf8(sqlString);  
+
+
+            DataSet ds = new DataSet();
+            try
+            {
+                MySqlDataAdapter da = new MySqlDataAdapter(sqlString, dbConnection);
+                da.Fill(ds);
+
+            }
+            catch (Exception ee)
+            {
+
+                throw new Exception("SQL:" + sqlString + "\n" + ee.Message.ToString());
+            }
+            return ds;
+
+        }
+        #endregion
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //计时器
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            label1.Text = DateTime.Now.ToString();
+
+            string nowTime = DateTime.Now.ToString("HH:mm:ss");
+
+
+            if (nowTime.Equals(textBox1.Text) && pos == 1)
+            {
+                output(DateTime.Now.ToString());
+                output("时间已到,开始发送！！！");
+                SendEmail(DateTime.Now.ToString(), Mysql());
+            }
+
+        }
+
+        //日志输出窗口
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //日志输出
+        public void output(string log)
+        {
+            textBox2.AppendText(log + "\r\n");
+        }
+
+        //停止计时
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pos = 0;//标志位为0，停止执行
+            output("停止计时");
+        }
+
+        //实时显示时间
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
